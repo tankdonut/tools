b4d07b7694b6cd35ef62a5b274268c62341e9fc0
tankdonut <206458089+tankdonut@users.noreply.github.com>
2026-04-23 03:40:24 +0000

    feat(tasks): register release invoke task in collection

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,7 +1,8 @@
 from invoke.collection import Collection
 
-from tasks import build, tools
+from tasks import build, release, tools
 
 ns = Collection()
 ns.add_collection(Collection.from_module(build))
+ns.add_collection(Collection.from_module(release))
 ns.add_collection(Collection.from_module(tools))
